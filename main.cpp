8dd5babc5572f4243e0f956b65d5fba831fbc86c
Jorge Francisco Sanchez <jor.ge.fr.sa.22@gmail.com>
2019-05-02 20:02:43 -0500

    Agrego documentacion inicial

--- main.cpp
+++ main.cpp
@@ -1,11 +1,22 @@
 #include "mainwindow.h"
 #include <QApplication>
+#include <QSplashScreen>
+#include <QThread>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
+
+    QPixmap pixmap("/hawkview.png");
+         QSplashScreen splash(pixmap);
+         splash.show();
+         a.thread()->sleep(2);
+         a.processEvents();
+         QMainWindow window;
+         w.show();
+         splash.finish(&window);
 
     return a.exec();
 }
--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,11 +1,13 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QSplashScreen>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+   QWidget::showMaximized();
 }
 
 MainWindow::~MainWindow()
